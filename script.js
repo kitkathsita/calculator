d6195379d3555d447fbc6afc68c56b572b868644
kitkath <katherinpgr_99@hotmail.com>
2022-11-03 22:06:18 -0500

    i dont know what im doin

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const onOff = document.querySelector('.turn')
 const screen = document.querySelector('.screen')
+const textScreen = document.querySelector('.textScreen')
+const num = document.querySelectorAll('.number')
+const operatSign = document.querySelectorAll('.ope')
+
+let firstNum =  ''
+let secondNum = ''
+let operSign = ''
+
+let operation = {
+  firtsOperandu : '',
+  operat : ''
+}
 
 function adition(a, b) {
   return a + b
@@ -21,13 +33,13 @@ function operate(opp, a, b) {
   const operation = opp
   const num1 = a
   const num2 = b
-  if (operation === 'sum') {
+  if (operation === 'adition') {
     total = adition(num1, num2)
-  } else if (operation === 'res') {
+  } else if (operation === 'substract') {
     total = substraction(num1, num2)
-  } else if (operation === 'mul') {
+  } else if (operation === 'multiply') {
     total =  multiplication(num1, num2)
-  } else if (operation === 'div') {
+  } else if (operation === 'division') {
     total = division(num1, num2)
   } else {
     total = 'invalid operation'
@@ -36,12 +48,47 @@ function operate(opp, a, b) {
 }
 
 function initialCalc() {
-  var textInit = document.createElement('p')
-  screen.appendChild(textInit)
-  textInit.innerHTML = 'HELLO'
-  textInit.classList.add('animation')
+  let textInit = document.createElement('p')
+  textInit.innerHTML = ''
+  if (this.classList[1]==='on') {
+    // screen.appendChild(textInit)
+    // textInit.innerHTML = 'HELLO'
+    // textInit.classList.add('animation')
+    this.classList.remove('on')
+    this.classList.add('off')
+  } else {
+    // screen.appendChild(textInit)
+    // textInit.innerHTML = 'BYE'
+    // textInit.classList.add('animation')
+    this.classList.remove('off')
+    this.classList.add('on')
+  }
+  
+}
+
+function printNums(e) {
+  if (onOff.classList[1]==='on') {
+    // var textInit = document.createElement('p')
+    firstNum = firstNum + e.target.innerText
+    textScreen.classList.add('styleText')
+    textScreen.innerText = firstNum
+    //console.log(this.innerText)
+    if (operation.firtsOperandu === '') {
+      operation.firtsOperandu = e.target.innerText
+    } 
+}
+}
+
+function saveOperation(e) {
+  if (onOff.classList[1]==='on') {
+    operSign = e.target.innerText
+    textScreen.innerText = textScreen.innerText+operSign
+  }
 }
 
 console.log(operate('div', 5, 5))
 
 onOff.addEventListener('click', initialCalc)
+
+num.forEach(number => number.addEventListener('click', printNums))
+operatSign.forEach(sign => sign.addEventListener('click', saveOperation))
