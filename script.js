6b9237b22598f18a9316d3a2eda4b59f0adda0c9
kitkath <katherinpgr_99@hotmail.com>
2022-11-04 22:45:19 -0500

    now it works, it makes operations, rounds number, makes multiple operations, and the AC button works

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const screen = document.querySelector('.screen')
 const textScreen = document.querySelector('.textScreen')
 const num = document.querySelectorAll('.number')
 const operatSign = document.querySelectorAll('.ope')
+const equal = document.querySelector('.equal')
+const allClear = document.querySelector('.ac')
 
 let firstNum =  ''
 let secondNum = ''
@@ -29,10 +31,11 @@ function division(a, b) {
   return a / b
 }
 
-function operate(opp, a, b) {
-  const operation = opp
-  const num1 = a
-  const num2 = b
+function operate() {
+  const operation = operSign
+  const num1 = Number(firstNum)
+  const num2 = Number(secondNum)
+  console.log(operation)
   if (operation === 'adition') {
     total = adition(num1, num2)
   } else if (operation === 'substract') {
@@ -44,7 +47,10 @@ function operate(opp, a, b) {
   } else {
     total = 'invalid operation'
   }
-  return total
+  textScreen.innerText = Math.round(total*1000)/1000
+  firstNum = total
+  operSign = ''
+  secondNum = ''
 }
 
 function initialCalc() {
@@ -68,27 +74,55 @@ function initialCalc() {
 
 function printNums(e) {
   if (onOff.classList[1]==='on') {
-    // var textInit = document.createElement('p')
-    firstNum = firstNum + e.target.innerText
-    textScreen.classList.add('styleText')
-    textScreen.innerText = firstNum
-    //console.log(this.innerText)
-    if (operation.firtsOperandu === '') {
-      operation.firtsOperandu = e.target.innerText
+    if (operSign === '') {
+      firstNum = firstNum + e.target.innerText
+      textScreen.classList.add('styleText')
+      console.log(firstNum)
+      textScreen.innerText = firstNum
+    } else {
+      secondNum = secondNum + e.target.innerText
+      textScreen.innerText = textScreen.innerText + e.target.innerText
+      console.log(secondNum)
     } 
 }
 }
 
 function saveOperation(e) {
   if (onOff.classList[1]==='on') {
-    operSign = e.target.innerText
-    textScreen.innerText = textScreen.innerText+operSign
+    if (operSign === '') {
+      operSign = this.classList[1]
+      console.log(typeof(operSign))
+      textScreen.innerText = textScreen.innerText + e.target.innerText
+    } else {
+      operate()
+      operSign = this.classList[1]
+      textScreen.innerText = textScreen.innerText + e.target.innerText
+    }
   }
 }
 
-console.log(operate('div', 5, 5))
+function clearAll() {
+  firstNum = ''
+  secondNum = ''
+  operSign = ''
+  textScreen.innerText = ''
+}
+
+function deleteOne(){
+  if (secondNum != '') {
+    secondNum = secondNum.slice(-(secondNum.length-1), -1)
+  } else if (secondNum === '' && operSign != '') {
+    operSign = ''
+  } else {
+    firstNum = firstNum.slice(-(firstNum.length-1), -1)
+  }
+}
+
+//console.log(operate('div', 5, 5))
 
 onOff.addEventListener('click', initialCalc)
 
 num.forEach(number => number.addEventListener('click', printNums))
 operatSign.forEach(sign => sign.addEventListener('click', saveOperation))
+equal.addEventListener('click', operate)
+allClear.addEventListener('click', clearAll)
